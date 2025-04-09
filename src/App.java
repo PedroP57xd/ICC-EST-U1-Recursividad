3b49f1d2d508026361130fd83c34012083024757
PEDRO P <pedropanjon8745@gmail.com>
2025-04-09 16:17:53 -0500

    static

--- src/App.java
+++ src/App.java
@@ -1,5 +1,18 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    
+        int n=5;
+        int resultadoFinal=factorial(n);
+        System.out.println("resultado: " + resultadoFinal);
     }
+    public int factorial(int n){
+        if (n==0) {
+            System.out.println("Alcance el caso base");
+            return 1;
+        } 
+        int resultado =n*factorial(n-1);
+        System.out.println("Calculando factorial de "+ n+" = factorial("+(n-1)+")");
+        return resultado;
+    }
+
 }
